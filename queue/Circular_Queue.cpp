a6bc7e4f20f7b0955e7dd1574aed40422cea45a1
VinayakSingh2513465 <vinayaksingh4893@gmail.com>
2026-04-18 14:50:07 +0530

    done

---
+++ queue/Circular_Queue.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+
+using namespace std;
+
+class queue {
+    int* arr;
+
+    int capacity;
+    int currSize;
+
+    int f,r;
+
+public:
+    queue(int capacity) {
+        this->capacity = capacity;
+        arr=new int[capacity];
+        currSize = 0;
+        f=0;
+        r=-1;
+    }
+
+    void push(int data) {
+        if (currSize == capacity) {
+            cout<<"queue is FULL\n";
+            return;
+        }
+        r=(r+1)%capacity;
+        arr[r]=data;
+        currSize++;
+    }
+
+    void pop() {
+        if (empty()) {
+            cout<<"Queue is EMPTY\n";
+            return;
+        }
+        f=(f+1)%capacity;
+        currSize--;
+    }
+    int front() {
+        if (empty()) {
+            cout<<"Queue is EMPTY\n";
+            return -1;
+        }
+        return arr[r];
+    }
+
+    bool empty() {
+        return currSize==0;
+    }
+
+};
+
+int main() {
+    queue q(4);
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+
+    cout<<q.front()<<endl;
+    q.pop();
+    cout<<q.front()<<endl;
+    q.push(5);
+    cout<<q.front()<<endl;
+    return 0;
+}
